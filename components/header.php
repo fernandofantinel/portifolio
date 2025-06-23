6e69ec0a202ec73e3a672a4aed71408a472636be
Fernando Fantinel <fantinel89@gmail.com>
2025-06-23 19:40:27 -0300

    feat: add header section

---
+++ components/header.php
@@ -0,0 +1,21 @@
+<header class="w-[1366px] h-[682px] flex flex-col gap-20 items-center">
+  <div class="flex flex-col gap-14 items-center">
+    <div class="h-28 w-28 rounded-full overflow-hidden border-2 border-main-red mt-30">
+      <img class="w-full h-full object-cover object-center" src="../assets/img/photo.jpg" alt="Foto de Fernando Fantinel">
+    </div>
+    <div class="flex flex-col gap-5 text-center">
+      <div class="flex flex-col items-center">
+        <div class="text-gray-200 text-[20px]">Olá, meu nome é <span class="text-main-red">Fernando Fantinel</span> e sou</div>
+        <div class="text-gray-100 font-bold text-[56px] leading-[1.2]">Desenvolvedor PHP</div>
+      </div>
+      <div class="text-gray-300 text-sm w-[680px]">Transformo necessidades em aplicações reais, evolventes e funcionais. Desenvolvo sistemas através da minha paixão pela tecnologia, contribuindo com soluções inovadoras e eficazes para desafios complexos.</div>
+    </div>
+  </div>
+  <div class="flex gap-3">
+    <div class="bg-main-purple px-3 py-1 gap-2 rounded-full font-bold leading-[1.2]">PHP</div>
+    <div class="bg-main-red px-3 py-1 gap-2 rounded-full font-bold leading-[1.2]">Laravel</div>
+    <div class="bg-main-yellow px-3 py-1 gap-2 rounded-full font-bold leading-[1.2]">JavaScript</div>
+    <div class="bg-main-green px-3 py-1 gap-2 rounded-full font-bold leading-[1.2]">HTML</div>
+    <div class="bg-main-blue px-3 py-1 gap-2 rounded-full font-bold leading-[1.2]">Tailwind CSS</div>
+  </div>
+</header>
