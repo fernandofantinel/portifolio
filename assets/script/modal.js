3a5449fe7ffc6411dba7f4b8ddf5cee8b54c5cc0
Fernando Fantinel <fantinel89@gmail.com>
2025-06-26 15:31:31 -0300

    feat: show the appropriate modal screenshot for each project

--- assets/script/modal.js
+++ assets/script/modal.js
@@ -1,4 +1,16 @@
 const modal = document.getElementById("modal");
+const thumbnails = document.querySelectorAll(".thumb");
+const modalImage = document.getElementById("modal-image");
+
+thumbnails.forEach((thumb) => {
+  thumb.addEventListener("click", () => {
+    const src = thumb.getAttribute("src");
+    modalImage.setAttribute("src", src);
+  
+    const alt = thumb.getAttribute("alt");
+    modalImage.setAttribute("alt", alt);
+  });
+});
 
 document.querySelectorAll(".openModal").forEach(function (element) {
   element.addEventListener("click", function () {
--- components/modal.php
+++ components/modal.php
@@ -3,7 +3,7 @@
   <div class="w-full h-full relative bg-gray-950 pt-20">
     <button id="closeModal" class="absolute top-2 right-10 text-gray-100 hover:text-red-600 text-2xl font-bold cursor-pointer">&times;</button>
     <div class="flex justify-center px-4 py-4">
-      <img class="h-[510px] rounded-2xl" src="../assets/img/portfolio_thumb.png" alt="">
+      <img id="modal-image" class="h-[510px] rounded-2xl" src="" alt="">
     </div>
   </div>
 </div>
