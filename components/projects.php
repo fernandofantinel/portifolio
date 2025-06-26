4079ca0e8672372497739a10e36804dea7601a5b
Fernando Fantinel <fantinel89@gmail.com>
2025-06-26 19:56:18 -0300

    feat: add projects url and github link

--- components/projects.php
+++ components/projects.php
@@ -5,6 +5,8 @@ $projects = [
     'alt' => 'Foto do projeto do portfólio',
     'title' => 'Portfólio',
     'description' => 'Site para exibir os projetos realizados até o momento.',
+    'url' => 'https://portfolio.fantinel.dev.br',
+    'github' => 'https://github.com/fernandofantinel/portifolio',
     'technologies' => [
       [
         'name' => 'PHP',
@@ -25,6 +27,8 @@ $projects = [
     'alt' => 'Foto do projeto Clone Tabnews',
     'title' => 'Clone Tabnews',
     'description' => 'Recriação do zero do tabnews.com.br. Projeto feito no curso.dev.',
+    'url' => 'https://clone-tabnews.fantinel.dev.br/api/v1/status',
+    'github' => 'https://github.com/fernandofantinel/clone-tabnews',
     'technologies' => [
       [
         'name' => 'JavaScript',
@@ -53,6 +57,8 @@ $projects = [
     'alt' => 'Foto da página pessoal Devlinks',
     'title' => 'Página pessoal',
     'description' => 'Página pessoal para exibição dos contatos.',
+    'url' => 'https://fantinel.dev.br/',
+    'github' => 'https://github.com/fernandofantinel/devlinks',
     'technologies' => [
       [
         'name' => 'CSS',
@@ -73,6 +79,8 @@ $projects = [
     'alt' => 'Foto da página Karaoke Roulette',
     'title' => 'Karaoke Roulette',
     'description' => 'Projeto criado para se divertir cantando com amigos.',
+    'url' => 'https://karaoke-roulette.vercel.app/',
+    'github' => 'https://github.com/fernandofantinel/karaoke-roulette',
     'technologies' => [
       [
         'name' => 'ReactJS',
@@ -107,7 +115,14 @@ $projects = [
     </div>
     <div class="flex flex-1 flex-col min-w-0 h-full justify-between">
       <div class="flex flex-col gap-2">
-        <span class="text-gray-100 font-bold leading-tight"><?= $project['title']; ?></span>
+        <div class="flex justify-between">
+          <a href="<?= $project['url']; ?>" target="_blank">
+            <span class="text-gray-100 font-bold leading-tight"><?= $project['title']; ?></span>
+          </a>
+          <a href="<?= $project['github']; ?>" target="_blank">
+            <img class="text-gray-300" src="../assets/img/github-logo.svg" alt="Logo do GitHub">
+          </a>
+        </div>
         <p class="text-gray-200 text-sm leading-normal"><?= $project['description']; ?></p>
       </div>
       <div class="flex flex-wrap gap-2">
